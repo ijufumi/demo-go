33c383bbabf0ac361ca82e5ba5549555c6e07fe5
Takafumi Iju <ijufumi@gmail.com>
2019-08-16 13:01:35 +0900

    Delete unnecessary code.

--- api_client/api/private/open_position.go
+++ api_client/api/private/open_position.go
@@ -32,9 +32,5 @@ func (c *openPosition) OpenPositions(symbol configuration.Symbol, pageNo int) (*
 		return nil, fmt.Errorf("%v", opensPositionRes.Messages)
 	}
 
-	positionID := make([]int64, 0, len(opensPositionRes.Data.List))
-	for _, p := range opensPositionRes.Data.List {
-		positionID = append(positionID, p.PositionID)
-	}
 	return opensPositionRes, nil
 }
