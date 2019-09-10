5cc34a4a2fe034fc3f924c73e2bd0154c9a3745e
Takafumi Iju <ijufumi@gmail.com>
2019-09-10 18:37:27 +0900

    Add factory method of private client.

--- api_client/api/private/client.go
+++ api_client/api/private/client.go
@@ -2,6 +2,7 @@ package private
 
 import (
 	"api_client/api/private/internal/connect"
+	"os"
 )
 
 // Client ...
@@ -23,6 +24,22 @@ type client struct {
 
 // New create Client instance.
 func New(apiKey, secretKey string) Client {
+	c := &client{}
+	con := connect.New(apiKey, secretKey)
+	c.order.con = con
+	c.activeOrders.con = con
+	c.cancelOrder.con = con
+	c.closeOrder.con = con
+	c.openPositions.con = con
+
+	return c
+}
+
+// NewWithEnv ...
+func NewWithEnv() Client {
+	apiKey := os.Getenv("API_KEY")
+	secretKey := os.Getenv("API_SECRET")
+
 	c := &client{}
 	con := connect.New(apiKey, secretKey)
 	c.order.con = con
