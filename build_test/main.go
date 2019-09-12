f77a2a72eec7b3fea65d03682646c1d07605a3f6
Takafumi Iju <ijufumi@gmail.com>
2019-09-12 20:55:58 +0900

    Modify code and remove binary.

--- build_test/main.go
+++ build_test/main.go
@@ -1,13 +1,23 @@
 package main
 
-import (
-	"log"
+import "log"
+
+const (
+	test = "00000000000"
 )
 
 var (
+	aaa = "9999999999"
 	key string
 )
 
 func main() {
+	hoge(12345)
+}
+
+func hoge(a int) {
 	log.Printf("key is %v", key)
+	log.Printf("test is %v", test)
+	log.Printf("aaa is %v", aaa)
+	log.Printf("a is %v", a)
 }
