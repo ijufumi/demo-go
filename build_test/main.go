d79bb9e73c18f1b15f8950114e762a35336e6af1
Takafumi Iju <ijufumi@gmail.com>
2019-09-13 20:51:56 +0900

    Modify test code.

--- build_test/main.go
+++ build_test/main.go
@@ -11,13 +11,25 @@ var (
 	key string
 )
 
+type fuga struct {
+	a string
+	b string
+}
+
 func main() {
+	f := fuga{
+		a: "123",
+		b: "456",
+	}
 	hoge(12345)
+	log.Println(f)
 }
 
 func hoge(a int) {
+	s := "abcdefg"
 	log.Printf("key is %v", key)
 	log.Printf("test is %v", test)
 	log.Printf("aaa is %v", aaa)
 	log.Printf("a is %v", a)
+	log.Printf("s is %v", s)
 }
